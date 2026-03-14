ebc1b18b388a7e4edcc1a42b4c060787a377b6eb
ECKO <None@email.com>
2026-03-14 16:28:10 +0900

    Version 1

--- Content.Client/Silicons/Borgs/BorgSelectTypeUserInterface.cs
+++ Content.Client/Silicons/Borgs/BorgSelectTypeUserInterface.cs
@@ -1,3 +1,4 @@
+using Content.Shared.Books;
 using Content.Shared.Silicons.Borgs.Components;
 using JetBrains.Annotations;
 using Robust.Client.UserInterface;
@@ -23,8 +24,12 @@ public sealed class BorgSelectTypeUserInterface : BoundUserInterface
     protected override void Open()
     {
         base.Open();
+        //Mono: Selectable borg whitelist
+        EntMan.TryGetComponent<BorgSwitchableTypeComponent>(Owner, out var comp);
+        var whitelist = comp?.TypeWhitelist;
 
         _menu = this.CreateWindow<BorgSelectTypeMenu>();
+        _menu.Populate(whitelist); //Mono
         _menu.ConfirmedBorgType += (prototype, subtype) => SendPredictedMessage(new BorgSelectTypeMessage(prototype, subtype));
     }
 }
